6f96c5db48d7e88337a3a5fbc68362f486aa16dd
Mu <jasonhemu@users.noreply.github.com>
2019-04-05 14:25:37 -0400

    Create download_site_roster.js

---
+++ download_site_roster.js
@@ -0,0 +1,194 @@
+// ==UserScript==
+// @name        Download Site Roster
+// @description Generates a .CSV download of the site roster for all students
+// @include     https://*.instructure.com/courses/*/users
+// @require     https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/1.3.3/FileSaver.js
+// @version     0.1
+// @grant       none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var studentArr = [];
+    var pending = -1;
+    var fetched = 0;
+    var needsFetched = 0;
+    var ajaxPool;
+    addButton();
+
+    function addButton() {
+        if ($('#site_roster').length === 0) {
+            $('#people-options > ul').append(
+                '<li class="ui-menu-item" role="presentation">'
+                + '<a id="site_roster" class="ui-corner-all" role="menuitem" tabindex="-1" style="cursor: pointer;">'
+                + '<i class="icon-collection-save"></i> Download Roster</a></li>');
+            $('#site_roster').one('click', roster);
+        }
+        return;
+    }
+
+    function setupPool() {
+        try {
+            ajaxPool = [];
+            $.ajaxSetup({
+                'beforeSend' : function(jqXHR) {
+                    ajaxPool.push(jqXHR);
+                },
+                'complete' : function(jqXHR) {
+                    var i = ajaxPool.indexOf(jqXHR);
+                    if (i > -1) {
+                        ajaxPool.splice(i, 1);
+                    }
+                }
+            });
+        } catch (e) {
+            throw new Error('Error configuring AJAX pool');
+        }
+    }
+
+    function getCourseId() {
+        var courseId = null;
+        try {
+            var courseRegex = new RegExp('/courses/([0-9]+)');
+            var matches = courseRegex.exec(window.location.href);
+            if (matches) {
+                courseId = matches[1];
+            } else {
+                throw new Error('Unable to detect Course ID');
+            }
+        } catch (e) {
+            errorHandler(e);
+        }
+        return courseId;
+    }
+
+    function roster() {
+        setupPool();
+        var courseId = getCourseId();
+        var url = '/api/v1/courses/' + courseId + '/users?enrollment_type%5b%5d=student&include%5b%5d=email&include%5b%5d=enrollments&per_page=100';
+        pending = 0;
+        notification();
+        getRoster(courseId, url);
+    }
+
+    function nextURL(linkTxt) {
+        var url = null;
+        if (linkTxt) {
+            var links = linkTxt.split(',');
+            var nextRegEx = new RegExp('^<(.*)>; rel="next"$');
+            for (var i = 0; i < links.length; i++) {
+                var matches = nextRegEx.exec(links[i]);
+                if (matches) {
+                    url = matches[1];
+                }
+            }
+        }
+        return url;
+    }
+
+    function getRoster(courseId, url) {
+        try {
+            pending++;
+            $.getJSON(url, function(udata, status, jqXHR) {
+                url = nextURL(jqXHR.getResponseHeader('Link'));
+                for (var i = 0; i < udata.length; i++) {
+                    var student = udata[i];
+                    if (student.email && student.email.length > 0 && student.sis_user_id && student.name && student.enrollments) {
+                        var studentData = {};
+                        studentData.name = student.name;
+                        studentData.id = student.id;
+                        studentData.sis_user_id = student.sis_user_id;
+                        studentData.sis_login_id = student.sis_user_id;
+                        studentData.section = student.enrollments[0].sis_section_id;
+                        studentData.email = student.email;
+                    }
+                    studentArr.push(studentData);
+                }
+                if (url) {
+                    getRoster(courseId, url);
+                }
+                pending--;
+                if (pending <= 0 && studentArr.length >= 1) {
+                    makeCSV();
+                }
+            }).fail(function() {
+                throw new Error('Failed to load list of students');
+            });
+        } catch (e) {
+            errorHandler(e);
+        }
+    }
+
+    function makeCSV() {
+        try {
+            var csv = createCSV();
+            if (csv) {
+                var blob = new Blob([ csv ], {
+                    'type' : 'text/csv;charset=utf-8'
+                });
+                var courseId = getCourseId();
+                saveAs(blob, 'roster-' + courseId + '.csv');
+                $('#site_roster').one('click', roster);
+            } else {
+                throw new Error('Problem creating report');
+            }
+        } catch (e) {
+            errorHandler(e);
+        }
+    }
+
+    function createCSV() {
+        var fields = [ {
+            'name' : 'Student',
+            'src' : 'u.name'
+        }, {
+            'name' : 'ID',
+            'src' : 'u.id'
+        }, {
+            'name' : 'SIS User ID',
+            'src' : 'u.sis_user_id'
+        }, {
+            'name' : 'SIS Login ID',
+            'src' : 'u.sis_user_id'
+        }, {
+            'name' : 'Section',
+            'src' : 'u.section'
+        }, {
+            'name' : 'Email',
+            'src' : 'u.email'
+        } ];
+
+        var CRLF = '\r\n';
+        var hdr = ['Student', 'ID', 'SIS User ID', 'SIS Login ID', 'Section', 'Email'];
+        var t = hdr.join(',') + CRLF;
+        for (var i = 0; i < studentArr.length; i++) {
+            var item = [];
+            for (var key in studentArr[i]) {
+                item.push(studentArr[i][key]);
+            }
+            t += item.join(',') + CRLF;
+        }
+        return t;
+    }
+
+    function notification() {
+        $('body').append('<div id="roster_notification"></div>');
+        $('#roster_notification').html('<p>The site roster export is completed. Please check your download folder.</p>');
+        $('#roster_notification').dialog({
+            'title' : 'Fetching Site Roster',
+            'autoOpen' : true,
+            'buttons' : [ {
+                'text' : 'Close',
+                'click' : function() {
+                    $(this).dialog('close');
+                    $('#notification').one('click', roster);
+                }
+            } ]
+        });
+    }
+
+    function errorHandler(e) {
+        console.log(e.name + ': ' + e.message);
+    }
+})();
